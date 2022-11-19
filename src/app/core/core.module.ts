0bfaa6981a9ced1e5cf073ec91f1db0279d09cb2
Viraj02 <virujoshi02@gmail.com>
2022-11-19 20:00:03 +0530

    Imported material component in material module file.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,11 +5,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // Components
 import { RegistrationComponent } from './registration/registration.component';
 
-// Angular Material
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+
 import { LoginComponent } from './login/login.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -26,9 +24,7 @@ import { LoginComponent } from './login/login.component';
     CommonModule,
     ReactiveFormsModule,
     FormsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule
+    MaterialModule,
   ]
 })
 export class CoreModule { }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Angular Material
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+  ],
+  exports: [
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
   ]
 })
 export class MaterialModule { }
