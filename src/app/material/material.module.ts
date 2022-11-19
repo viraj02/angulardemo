a127a7a32efd6901b17ad3cf7a414193072825d6
Viraj02 <virujoshi02@gmail.com>
2022-11-19 21:09:54 +0530

    create array variable and assigned that value in import and export.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,23 +6,17 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+
+const MATERIAL_MODULE = [
+  MatInputModule, MatButtonModule, MatIconModule, MatToolbarModule, MatSidenavModule
+];
 
 
 @NgModule({
-  declarations: [
-  ],
-  imports: [
-    CommonModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatToolbarModule,
-  ],
-  exports: [
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatToolbarModule,
-  ]
+  declarations: [],
+  imports: MATERIAL_MODULE,
+  exports: MATERIAL_MODULE
 })
 export class MaterialModule { }
