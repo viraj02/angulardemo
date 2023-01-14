bc5ab8c9d33700a53e74071542e8e24073c8a0ee
Viraj02 <virujoshi02@gmail.com>
2023-01-14 16:00:30 +0530

    Created basic task input field

--- src/app/todo/main/main.component.ts
+++ src/app/todo/main/main.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  todoList: any = [];
+  todoInput: string | null = null;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  todoChange(event): void {
+    this.todoInput = event.target.value;
+  }
+
+  addNewTodoTask(): void {
+    if (this.todoInput) { this.todoList.push(this.todoInput); }
+    this.todoInput = null;
   }
 
 }
